253ff9b99144fa24a18e9440a7662a98da729cee
IgnacioMG <ignaciomaringarro@gmail.com>
2023-01-22 13:12:01 -0600

    Add instructions for each question

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 
 // 1. Respuesta pregunta 1
 
+//Indique los diferentes tipos de datos primitivos y dé un ejemplo de cada uno.
+
 // tipo Boolean que son los que son true o false
 // tipo number que claro son los numeros reales
 // tipo undefined que es el que es NaN
@@ -14,6 +16,9 @@
 
 // 2. Respuesta pregunta 2
 
+//Indique las 2 maneras con las cuales se pueden agregar propiedades nuevas
+// a un objeto, adjunte un ejemplo para cada caso.
+
 // utilizando el operador " propiedad " (comillas) 
 // con los [ propiedad ] (corchetes cuadrados)
 
@@ -21,6 +26,15 @@
 
 
 // 3. Respuesta pregunta 3
+
+//Explique qué función tiene el += en el siguiente fragmento de código, y qué
+// sería lo que muestra el console.log en cada caso.
+
+// let list = 1231
+// list += 3234
+// console.log(list)
+// list += 'textoDePrueba'
+// console.log(list)
 
 // lo que es el valor de "+=" es un operador para poder sumar lo siguiente o lo que es a la derecha
 //de un valor para agregar ese valor que da la suma a alguna variable
@@ -38,6 +52,18 @@
 
 // 4. Respuesta pregunta 4
 
+//Explique qué hace el operador != y !==, en cada caso, y el resultado de cada
+
+// A.
+// const flag = false
+// flag != true
+// flag !== true
+
+// B.
+// const num = 10
+// num !="10"
+// num !== "10"
+
 // para empezar el operador "!=" es un operador de desigualdad o sea que compara dos valores y si son
 // "true" te devuelve que son "tru" y si son "false" pues te devuelve "false"
 
@@ -52,6 +78,8 @@
 
 // 5. Respuesta pregunta 5
 
+//¿Qué es una función anónima?
+
 // No tengo muy claro que sera una funcion anonima mas me suena que debe ser algun tipo de funcion 
 // la cual no cuenta con un nombre asignado cuando la declaramos
 
@@ -59,6 +87,7 @@
 
 
 // 6. Respuesta pregunta 6
+//Explique la diferencia entre los function declaration y los function expression
 
 // una function declaration son las que se le da la palabra " function " seguido de un nombre
 //mas unos parentesis y llaves
@@ -70,6 +99,7 @@
 
 
 // 7. Respuesta pregunta 7
+//¿Qué es un método?
 
 // son los que permiten que un objeto tenfa acciones o realice algo como por el ejemplo los  "to string"
 
@@ -77,6 +107,7 @@
 
 
 // 8. Respuesta pregunta 8
+//Explique con sus propias palabras qué es recursividad.
 
 // la recursividad es cuando una funcion se empieza a llamar a ella misma para resolver algun problema
 // o que se divide como en pequenos problemas hasta que no se pueda llamar asi misma para realizar la acion
@@ -89,6 +120,16 @@
 // 1. Respuesta pregunta 1
 
 // Función para separar números pares e impares
+//Crear un programa que permita separar una lista de números aleatorios en
+// dos listas nuevas, de manera que se separen los valores en pares e impares.
+// Dicho programa debe recibir una lista de números como parámetro e iterar
+// dicha lista, de manera que se pueda verificar si el número es par o impar y
+// agrupar correctamente en la lista de números pares o impares. Al final debe
+// imprimirse ambas listas.
+
+// Importante: para resolver este programa debe hacer uso del ciclo for,
+// además debe usar funciones. Los nombres de las variables y funciones que
+// defina deben ser claros.
 
 function evenOdd(number) {
   let even = []
@@ -112,6 +153,13 @@ evenOdd(number)
 
 
 // 2. Respuesta pregunta 2
+
+// Dado la siguiente lista de usuarios, debe iterar cada uno e imprimir en el html,
+// dentro del div con el ID usuarios, los siguientes datos:
+// a. Dentro de un <p> el nombre del usuario y el apellido
+// b. Dentro de otro <p> el cargo que ocupa actualmente
+// c. Dentro de un <a> el link de social media correspondiente únicamente a
+// Facebook
 
 const usuarios = [
   {
