dd145a80ac7ea2d11834988650bd04c077d57e49
IgnacioMG <ignaciomaringarro@gmail.com>
2023-01-28 15:41:03 -0600

    class2
    Exercises to reverse a number, change the first letter of a word to uppercase, and as a personal challenge, change the first letter of each word in a sentence

---
+++ class2.js
@@ -0,0 +1,31 @@
+// 1. Escriba una function que escriba un número a la inversa
+function numberReverse(number) {
+  return number.toString().split('').reverse('').join('');
+}
+console.log(numberReverse(12345))
+
+// 2. Escriba una function que acepte un string como parámetro y 
+// convierta cada primer letra de cada palabra en mayúscula
+function leterUpper(word) {
+  let result = ""
+  for (let i = 0; i < word.length; i++) {
+    if (i === 0) {
+      result += word[i].toUpperCase()
+    } else {
+      result += word[i].toLowerCase()
+    }
+  }
+  return result
+}
+console.log(leterUpper('ignacio marin'))
+
+
+function leterUpper2(word2) {
+  const validation = word2.split('')
+  console.log(validation)
+  for (i = 0; i < validation.length; i++) {
+    const pModified = validation[0].toUpperCase()
+    return pModified
+  }
+}
+console.log(leterUpper2('ignacio marin'))
