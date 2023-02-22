109268bc3a2de5d3463981b8509f90297f9db251
IgnacioMG <ignaciomaringarro@gmail.com>
2023-02-22 11:03:43 -0600

    class 4 update
    The commit before this one wasn't finished correctly. This is the first correct solution.

--- class4.js
+++ class4.js
@@ -1,65 +1,65 @@
-const product = {
-  name: 'productName',
-  price: 200,
-  quantity: 50,
-  tax: 13,
-  category: ['home'],
-  subCategory: 'furniture',
-  total: function () {
-    return this.price + (this.price * this.tax / 100);
-  },
-};
+// const product = {
+//   name: 'productName',
+//   price: 200,
+//   quantity: 50,
+//   tax: 13,
+//   category: ['home'],
+//   subCategory: 'furniture',
+//   total: function () {
+//     return this.price + (this.price * this.tax / 100);
+//   },
+// };
 
-product.nuevoKey = 'es un nuevo key';
-product.category.push('xcategory');
-delete product.subCategory;
+// product.nuevoKey = 'es un nuevo key';
+// product.category.push('xcategory');
+// delete product.subCategory;
 
-function estudianteNuevo(nombre, apellidos, fechaNacimiento, id, nuevoIngreso, carrera, cursosMatriculados, cursosCursados) {
-  const datos = {};
+// function estudianteNuevo(nombre, apellidos, fechaNacimiento, id, nuevoIngreso, carrera, cursosMatriculados, cursosCursados) {
+//   const datos = {};
 
-  datos.nombre = nombre;
-  datos.apellidos = apellidos;
-  datos.fechaNacimiento = fechaNacimiento;
-  datos.id = id;
-  datos.nuevoIngreso = nuevoIngreso;
-  datos.carrera = carrera;
-  datos.cursosMatriculados = cursosMatriculados || [];
-  datos.cursosCursados = cursosCursados || [];
-  datos.cursosAprobados = [];
+//   datos.nombre = nombre;
+//   datos.apellidos = apellidos;
+//   datos.fechaNacimiento = fechaNacimiento;
+//   datos.id = id;
+//   datos.nuevoIngreso = nuevoIngreso;
+//   datos.carrera = carrera;
+//   datos.cursosMatriculados = cursosMatriculados || [];
+//   datos.cursosCursados = cursosCursados || [];
+//   datos.cursosAprobados = [];
 
 
-  datos.matricular = function (curso) {
-    this.cursosMatriculados.push(curso);
-  };
+//   datos.matricular = function (curso) {
+//     this.cursosMatriculados.push(curso);
+//   };
 
-  datos.retirarMatricula = function (curso) {
-    const index = this.cursosMatriculados.indexOf(curso);
-    if (index > -1) {
-      this.cursosMatriculados.splice(index, 1);
-    }
-  };
+//   datos.retirarMatricula = function (curso) {
+//     const index = this.cursosMatriculados.indexOf(curso);
+//     if (index > -1) {
+//       this.cursosMatriculados.splice(index, 1);
+//     }
+//   };
 
-  datos.estadoCurso = function(curso){
-    if (curso.nota >= 70){
-      curso.estado = "aprobado"
-    }else{
-      curso.estado = "reprobado"
-    }
-    this.cursosAprobados.push(curso)
-  }
+//   datos.estadoCurso = function(curso){
+//     if (curso.nota >= 70){
+//       curso.estado = "aprobado"
+//     }else{
+//       curso.estado = "reprobado"
+//     }
+//     this.cursosAprobados.push(curso)
+//   }
 
-  return datos;
-}
+//   return datos;
+// }
 
-const andy = estudianteNuevo('Andy', 'Smith', '1990-10-30', 'a-0001', true,
-  'Historía del Arte', ['Introducción a la pintura']);
-const ana = estudianteNuevo('Ana', 'Patel', '1990-10-30', 'arq-0001', true,
-  'Arquitectura', ['Introducción a la pintura', 'Historia del Arte I']);
+// const andy = estudianteNuevo('Andy', 'Smith', '1990-10-30', 'a-0001', true,
+//   'Historía del Arte', ['Introducción a la pintura']);
+// const ana = estudianteNuevo('Ana', 'Patel', '1990-10-30', 'arq-0001', true,
+//   'Arquitectura', ['Introducción a la pintura', 'Historia del Arte I']);
 
-const listaEstudiantes = [andy, ana];
+// const listaEstudiantes = [andy, ana];
 
-andy.matricular('Historia del Arte I');
-ana.retirarMatricula('Historia del Arte I');
+// andy.matricular('Historia del Arte I');
+// ana.retirarMatricula('Historia del Arte I');
 
 function EstudianteConstructor(nombre, apellidos, fechaNacimiento, id,
   nuevoIngreso, carrera, cursosMatriculados, cursosCursados, cursosAprobados) {
@@ -75,7 +75,7 @@ function EstudianteConstructor(nombre, apellidos, fechaNacimiento, id,
 
   this.matricular = function (curso) {
     this.cursosMatriculados.push(curso);
-  }; 
+  };
 
   this.retirarMatricula = function (curso) {
     const index = this.cursosMatriculados.indexOf(curso);
@@ -84,13 +84,13 @@ function EstudianteConstructor(nombre, apellidos, fechaNacimiento, id,
     }
   };
 
-  this.estadoCurso = function(curso) {
+  this.estadoCurso = function (curso) {
     if (curso.nota >= 70) {
       curso.estado = "aprobado"
     } else {
       curso.estado = "reprobado"
     }
-    this.cursosAprobados.push(curso)
+    this.cursosCursados.push(curso)
   }
 
 }
@@ -99,11 +99,11 @@ const estudiantePrueba = new EstudianteConstructor('Test', 'Testing', '1990-10-3
   'arq-0001', false, 'Arquitectura', ['Introducción a la pintura', 'Historia del Arte I'],
   [{ curso: 'Prueba', nota: 80 }, { curso: "mate", nota: 70 }]);
 
+estudiantePrueba.estadoCurso({ curso: 'JS', nota: 40 });
+estudiantePrueba.estadoCurso({ curso: 'mate', nota: 70 })
+
 console.log(estudiantePrueba);
 
-console.log("------------------------------")
-
-console.log(estudiantePrueba.estadoCurso)
 
 // 1. Crear un método que permita agregar cursos a la lista de cursos aprobados.
 // El usuario debe poder ingresar un objeto con el nombre del curso, nota.
